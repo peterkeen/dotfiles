e2f67147f14691779dac7d167ef5919e755e0fcb
Pete Keen <peter.keen@bugsplat.info>
2012-07-14 21:05:05 -0700

    do the variable comparison properly

--- notmuch/init.sh
+++ notmuch/init.sh
@@ -1,6 +1,6 @@
 __notmuch_count ()
 {
-    if $SHOW_NOTMUCH_COUNT; then
+    if [ -n "${SHOW_NOTMUCH_COUNT-}" ]; then
         count=`notmuch count --output=threads $1`
         if [[ $count > 0 ]]; then
             printf "[%s] " $count
