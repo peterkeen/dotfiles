d486e4f41fe0281af634b9f318205dedefddf540
Pete Keen <peter.keen@bugsplat.info>
2012-07-14 21:05:17 -0700

    define a custom ledger recipe

--- ledger/init.el
+++ ledger/init.el
@@ -1,4 +1,7 @@
-(require-el-get 'ledger-mode)
+(require-el-get
+ '(:name ledger
+         :type http
+         :url "https://raw.github.com/jwiegley/ledger/next/lisp/ledger.el"))
 
 (add-hook 'post-init-hook (lambda ()
     (setenv "LEDGER_FILE" "~/financials/ledger.txt")))
