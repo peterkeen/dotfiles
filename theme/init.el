0b077708792475a09d676f3913c6cf06fae923c3
Pete Keen <peter.keen@bugsplat.info>
2012-07-14 21:29:42 -0700

    Add day and night functions for theme

--- theme/init.el
+++ theme/init.el
@@ -1,6 +1,15 @@
 (require-el-get '(:name color-theme-solarized))
 
-(defun theme-init ()
+(defun night ()
+  (interactive)
+  (color-theme-solarized-dark))
+
+(defun day ()
+  (interactive)
   (color-theme-solarized-light))
+
+(defun theme-init ()
+  (day))
+
 
 (add-hook 'post-init-hook 'theme-init)
