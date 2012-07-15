f934f80dfb80b4ddc31c77943291063793b9b4af
Pete Keen <peter.keen@bugsplat.info>
2012-07-14 21:04:49 -0700

    stupid typo

--- markdown/init.el
+++ markdown/init.el
@@ -1,5 +1,5 @@
 (require-el-get 'markdown-mode)
 
-(after-hook 'post-init-hook (lambda ()
+(add-hook 'post-init-hook (lambda ()
   (setq auto-mode-list
         (cons '("\\.md" . markdown-mode) auto-mode-alist))))
