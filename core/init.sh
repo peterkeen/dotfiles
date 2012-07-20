dad7cfa0744061bdbab3f6aa5aa3481a259a318c
Pete Keen <peter.keen@bugsplat.info>
2012-07-19 19:49:08 -0700

    Change path to put /usr/local before /usr

--- core/init.sh
+++ core/init.sh
@@ -1,5 +1,5 @@
 unset PROMPT_COMMAND
 export EDITOR='/Applications/Emacs.app/Contents/MacOS/bin/emacsclient'
-export PATH=$PATH:/usr/local/bin:/usr/local/sbin
+export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 
 . ~/dotfiles/core/aliases.sh
