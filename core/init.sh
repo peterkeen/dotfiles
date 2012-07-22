7172b2552c35a71a2901be703bcc0fff64301a36
Pete Keen <peter.keen@bugsplat.info>
2012-07-22 06:33:21 -0700

    Only print if there are actually changes

--- core/init.sh
+++ core/init.sh
@@ -5,7 +5,10 @@ export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 
 __dotfiles_status ()
 {
-    printf "%s " `cd ~/dotfiles && rake show_differences_stat`
+    diffs=`cd ~/dotfiles && rake show_differences_stat`
+    if [[ $diffs != "" ]]; then
+        printf "%s " $diffs
+    fi
 }
 
 . ~/dotfiles/core/aliases.sh
