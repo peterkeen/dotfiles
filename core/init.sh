feb26fa53be3e9336e992deae1f165833f707a1e
Pete Keen <peter.keen@bugsplat.info>
2012-07-21 19:51:00 -0700

    Add a __dotfiles_status function to the prompt

--- core/init.sh
+++ core/init.sh
@@ -2,4 +2,10 @@ unset PROMPT_COMMAND
 export EDITOR='/Applications/Emacs.app/Contents/MacOS/bin/emacsclient'
 export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 
+
+__dotfiles_status ()
+{
+    printf "%s" `rake -f ~/dotfiles/Rakefile show_differences_stat`
+}
+
 . ~/dotfiles/core/aliases.sh
