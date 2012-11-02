c86562c60921d40c10f211e468e59ffbb3b88150
Pete Keen <pkeen@kongregate.com>
2012-11-02 12:56:40 -0700

    install rbenv

---
+++ ruby/bin/install_rbenv.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
--- ruby/init.sh
+++ ruby/init.sh
@@ -1,3 +1,5 @@
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM
+#[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM
 
+export PATH="$HOME/.rbenv/bin:$PATH"
+[[ `which rbenv` != "" ]] && eval "$(rbenv init -)"
 
