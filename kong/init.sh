95cd7a8be6e6339742b6c945908291446f5447cd
Pete Keen <pkeen@kongregate.com>
2013-04-20 11:21:22 -0700

    Generalize KONGROOT, remove notmuch_count

--- kong/init.sh
+++ kong/init.sh
@@ -1,7 +1,7 @@
 export EMAIL='pkeen@kongregate.com'
 export GIT_COMMITTER_EMAIL=$EMAIL
 export GIT_AUTHOR_EMAIL=$EMAIL
-export KONGROOT="/Users/pkeen/devel/kongregate"
+export KONGROOT="$HOME/devel/kongregate"
 export SHOW_NOTMUCH_INBOX_COUNT=true
 
 if [[ -f $HOME/.kongpass ]]; then
