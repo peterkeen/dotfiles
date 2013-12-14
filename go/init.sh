8fa97e7a7b86838b275d05f5ea54695ce4a461bd
Pete Keen <peter.keen@bugsplat.info>
2013-12-13 20:34:52 -0500

    nope, just set goroot

--- go/init.sh
+++ go/init.sh
@@ -1,16 +1,1 @@
-program_exists () {
-	type "$1" &> /dev/null ;
-}
- 
- 
-if program_exists go; then
-	function setupGOROOT()
-	{
-		local GOPATH=`which go`
-		local GODIR=`dirname $GOPATH`
-		local GOPATH_BREW_RELATIVE=`readlink $GOPATH`
-		local GOPATH_BREW=`dirname $GOPATH_BREW_RELATIVE`
-		export GOROOT=`cd $GODIR; cd $GOPATH_BREW/..; pwd`
-	}
-	setupGOROOT
-fi
+export GOROOT=/usr/local/go
