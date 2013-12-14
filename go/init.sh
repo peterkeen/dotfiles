8eb03707e944fce89b3577d7f283887caaef3c52
Pete Keen <peter.keen@bugsplat.info>
2013-12-13 19:53:33 -0500

    config for go 1.2

--- go/init.sh
+++ go/init.sh
@@ -1,3 +1,16 @@
-export GOROOT=/usr/local/go
-export GOPATH=$HOME/devel/go
-export PATH=$HOME/devel/go/bin:/usr/local/go/bin:$PATH
+program_exists () {
+	type "$1" &> /dev/null ;
+}
+ 
+ 
+if program_exists go; then
+	function setupGOROOT()
+	{
+		local GOPATH=`which go`
+		local GODIR=`dirname $GOPATH`
+		local GOPATH_BREW_RELATIVE=`readlink $GOPATH`
+		local GOPATH_BREW=`dirname $GOPATH_BREW_RELATIVE`
+		export GOROOT=`cd $GODIR; cd $GOPATH_BREW/..; pwd`
+	}
+	setupGOROOT
+fi
