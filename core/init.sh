fe74a45a7aaf30edece6329f96e0599a8fe3539d
Pete Keen <pkeen@kongregate.com>
2014-09-11 16:41:23 -0400

    Add a bunch of missing features to bash

--- core/init.sh
+++ core/init.sh
@@ -2,6 +2,11 @@ unset PROMPT_COMMAND
 export PATH=/Applications/Emacs.app/Contents/MacOS/bin:/usr/local/bin:/usr/local/sbin:$PATH
 export EDITOR='emacsclient'
 
+shopt -s histappend
+shopt -s checkhash
+shopt -s cdspell
+
+export PROMPT_COMMAND='history -a'
 
 __dotfiles_status ()
 {
