8bfa02e9fb012876f332fb9249db6f552482a787
Pete Keen <peter.keen@bugsplat.info>
2015-03-19 14:54:04 -0400

    edited aliases

--- core/aliases.sh
+++ core/aliases.sh
@@ -9,6 +9,8 @@ alias vi='emacsclient-nt.sh'
 alias vim='emacsclient-nt.sh'
 alias marginalia="rvm system do marginalia"
 alias okapi='ledger -f /Users/peter/okapi/ledger.txt'
+alias ack=ag
+alias rack=/opt/boxen/homebrew/bin/ack
 
 if [[ `which ag` != "" ]]; then
     alias ack=ag
