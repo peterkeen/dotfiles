a5be22563c721865ca92edb61fbbcb37593c6c3d
Pete Keen <peter.keen@bugsplat.info>
2015-05-17 08:54:50 -0400

    add bofa to aliases

--- ledger/init.sh
+++ ledger/init.sh
@@ -1,6 +1,6 @@
 export LEDGER_FILE=~/financials/ledger.txt
 
-alias bal='ledger bal assets:schwab:checking liabilities:amex liabilities:chase:sp'
-alias clr='ledger bal --cleared --real assets:schwab:checking liabilities:amex liabilities:chase:sp'
-alias ucl='ledger reg --uncleared assets:schwab:checking liabilities:amex liabilities:chase:sp'
+alias bal='ledger bal assets:schwab:checking assets:bofa liabilities:amex liabilities:chase:sp'
+alias clr='ledger bal --cleared --real assets:schwab:checking assets:bofa liabilities:amex liabilities:chase:sp'
+alias ucl='ledger reg --uncleared assets:schwab:checking assets:bofa liabilities:amex liabilities:chase:sp'
 alias bills='bills.py "peter.keen@gmail.com" "eni889@yahoo.com"'
