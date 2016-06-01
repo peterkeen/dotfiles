b43f80cd1130efaff4f4a53c30fbb871e905dda9
Pete Keen <pete@petekeen.net>
2016-06-01 08:33:30 -0400

    bump

--- ledger/init.sh
+++ ledger/init.sh
@@ -1,6 +1,6 @@
 export LEDGER_FILE=~/financials/ledger.txt
 
 alias bal='ledger bal assets:schwab:checking assets:bofa liabilities:amex liabilities:chase:sp'
-alias clr='ledger bal --cleared --real assets:schwab:checking assets:bofa liabilities:amex liabilities:chase:sp'
+alias clr='ledger bal --cleared --real assets:pnc liabilities:chase'
 alias ucl='ledger reg --uncleared assets:schwab:checking assets:bofa liabilities:amex liabilities:chase:sp'
 alias bills='bills.py "peter.keen@gmail.com" "eni889@yahoo.com"'
