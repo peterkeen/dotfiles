4f287b12ee29197e91521b8cce95a3984bdbd80b
Pete Keen <pete@petekeen.net>
2021-04-27 15:08:48 -0400

    Add real-auto-safe

--- note/init.el
+++ note/init.el
@@ -1,9 +1,17 @@
+(require-el-get
+ '(:name real-auto-save
+         :type git
+         :options ("xzf")
+         :url "https://github.com/ChillarAnand/real-auto-save.git"))
+
 (defun pak-goto-end-of-devlog ()
   (find-file (expand-file-name "~/notes/devlog.md"))
   (git-auto-commit-mode t)
   (visual-line-mode t)
   (auto-revert-mode t)
   (markdown-toggle-wiki-links t)
+  (real-auto-save-mode t)
+  (setq real-auto-save-interval 5)
   (setq gac-automatically-push-p 'f)
   (setq gac-debounce-interval 30)
   (end-of-buffer))
